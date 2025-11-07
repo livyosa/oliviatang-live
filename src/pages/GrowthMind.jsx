8c4e473b1bb5ea535a6ef520038ce3b27a5fd51f
Olivia Tang <oliviatang.ot@gmail.com>
2025-11-07 14:16:33 -0800

    make GrowthMind page background white and add GrowthMindHero component

--- src/pages/GrowthMind.jsx
+++ src/pages/GrowthMind.jsx
@@ -4,7 +4,7 @@ import GrowthMindHero from "../components/GrowthMindHero";
 
 export default function GrowthMind() {
   return (
-    <div className="bg-[#fdf3dc] text-gray-800 min-h-screen">
+    <div className="bg-white text-gray-800 min-h-screen">
       <Header />
       <main className="max-w-[62.5rem] mx-auto px-4 py-16">
         <GrowthMindHero />
