e350af6c12efab058e4373928d68c63057edd132
Olivia Tang <145073618+livyosa@users.noreply.github.com>
2025-12-26 22:24:25 -0800

    Fix blank page routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/growthmind" element={<GrowthMind />} />
+      <Route path="*" element={<Home />} />    
     </Routes>
   );
 }
