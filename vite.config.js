17b546ef998aae93f0d3d596144f20c6f685caa8
Olivia Tang <oliviatang.ot@gmail.com>
2026-01-04 04:54:45 -0800

    force clean rebuild for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/oliviatang-live/",
+  base: "/",
   build: {
     rollupOptions: {
       output: {
